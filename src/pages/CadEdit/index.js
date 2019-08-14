1ffa08dc961c809fc5f073c14fe0c4be948d9b5a
Rafael Marques <fieldesigner@hotmail.com>
2019-08-14 09:59:52 -0300

    datepicker incorporado

--- src/pages/CadEdit/index.js
+++ src/pages/CadEdit/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import { MdControlPoint } from 'react-icons/md';
 import { toast } from 'react-toastify';
+import DatePicker from 'react-datepicker';
 import { Container } from './styles';
-import DateInput from '~/components/Datepicker';
+// import DateInput from '~/components/Datepicker';
 import BannerInput from './BannerInput';
 import history from '~/services/history';
 import api from '~/services/api';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 const schema = Yup.object().shape({
   title: Yup.string().required('O titulo é obrigatório'),
   description: Yup.string().required('A descrição é obrigatória'),
   location: Yup.string().required('A localização é obrigatória'),
   id_image: Yup.number(),
-  date: Yup.date(),
+  date: Yup.string(),
 });
 
 export default function CadEdit() {
+  const [startDate, setDate] = useState(new Date());
+  function handleChange(date) {
+    setDate(date);
+  }
+
   async function handleSubmit(data) {
     console.tron.log(data);
     try {
@@ -41,7 +49,17 @@ export default function CadEdit() {
         <Input name="title" type="text" placeholder="Titulo do evento" />
         <Input multiline name="description" placeholder="Descrição do evento" />
         <Input name="location" type="text" placeholder="Local do evento" />
-        <DateInput name="date" />
+        <DatePicker
+          id="date"
+          name="date"
+          selected={startDate}
+          onChange={handleChange}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          dateFormat="MMMM d, yyyy h:mm aa"
+          timeCaption="time"
+        />
         <button type="submit">
           <MdControlPoint size={20} color="#fff" />
           Salvar meetup
